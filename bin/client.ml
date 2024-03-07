bb3222c4e84053f494472fabac54b12afc46383f
lykimq <lykimq@gmail.com>
2024-03-07 10:27:17 +0100

    Modulize client

--- bin/client.ml
+++ bin/client.ml
@@ -1,8 +1,41 @@
 open Lwt
-let usage () =
-  Printf.printf "Usage: %s <server_address> <port>\n" Sys.argv.(0);
-  exit 1
 
+(* Function to parse command line arguments *)
+let parse_command_line () =
+  match Array.length Sys.argv with
+  | 3 ->
+      let server_addr = Sys.argv.(1) in
+      let port_str = Sys.argv.(2) in
+      let port_num = int_of_string port_str in
+      if port_num < 0 || port_num > 65535 then (
+        Printf.printf "Invalid port number: %s\n" port_str;
+        exit 1)
+      else (server_addr, port_num)
+  | _ ->
+      Printf.printf "Usage: %s <server_address> <port>\n" Sys.argv.(0);
+      exit 1
+
+(* Function to handle message communication *)
+let handle_message ic oc msg =
+  let open Lwt.Infix in
+  Lwt_io.write_line oc msg >>= fun () ->
+  Lwt_io.read_line ic >>= fun response ->
+  Logs_lwt.info (fun m -> m "Server response: %s" response) >>= fun () ->
+  return_unit
+
+(* Function to send messages from stdin *)
+let send_messages ic oc =
+  let rec send_messages_loop () =
+    Lwt_io.read_line_opt Lwt_io.stdin >>= function
+    | Some msg -> handle_message ic oc msg >>= fun () -> send_messages_loop ()
+    | None ->
+        Logs_lwt.info (fun m -> m "End of input, closing connection.")
+        >>= fun () ->
+        Lwt_io.close oc >>= fun () -> return_unit
+  in
+  send_messages_loop ()
+
+(* Function to establish connection to the server *)
 let connect_to_server server_addr port =
   let open Lwt.Infix in
   Lwt_unix.getaddrinfo server_addr (string_of_int port)
@@ -18,24 +51,6 @@ let connect_to_server server_addr port =
   let oc = Lwt_io.of_fd ~mode:Lwt_io.output socket in
   return (ic, oc)
 
-let handle_message ic oc msg =
-  let open Lwt.Infix in
-  Lwt_io.write_line oc msg >>= fun () ->
-  Lwt_io.read_line ic >>= fun response ->
-  Logs_lwt.info (fun m -> m "Server response: %s" response) >>= fun () ->
-  return_unit
-
-let send_messages ic oc =
-  let rec send_messages_loop () =
-    Lwt_io.read_line_opt Lwt_io.stdin >>= function
-    | Some msg -> handle_message ic oc msg >>= fun () -> send_messages_loop ()
-    | None ->
-        Logs_lwt.info (fun m -> m "End of input, closing connection.")
-        >>= fun () ->
-        Lwt_io.close oc >>= fun () -> return_unit
-  in
-  send_messages_loop ()
-
 let run_client server_addr port =
   let open Lwt.Infix in
   connect_to_server server_addr port >>= fun (ic, oc) -> send_messages ic oc
@@ -43,17 +58,7 @@ let run_client server_addr port =
 let main () =
   let () = Logs.set_reporter (Logs.format_reporter ()) in
   let () = Logs.set_level (Some Logs.Info) in
-
-  (* Check command-line arguments *)
-  match Array.length Sys.argv with
-  | 3 ->
-      let server_addr = Sys.argv.(1) in
-      let port_str = Sys.argv.(2) in
-      let port_num = int_of_string port_str in
-      if port_num < 0 || port_num > 65535 then (
-        Printf.printf "Invalid port number: %s\n" port_str;
-        usage ())
-      else Lwt_main.run (run_client server_addr port_num)
-  | _ -> usage ()
+  let server_addr, port = parse_command_line () in
+  Lwt_main.run (run_client server_addr port)
 
 let () = main ()
