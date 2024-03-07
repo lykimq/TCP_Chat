8f6cab9f38465c703011818e8523569dd39a50af
lykimq <lykimq@gmail.com>
2024-03-07 17:54:41 +0100

    Add cmdliner in main

--- Readme.md
+++ Readme.md
@@ -4,8 +4,8 @@ To build and run at the top-level:
 
 ```bash
 dune build bin/main.exe
-_build/default/bin/main.exe server localhost 9000
-_build/default/bin/main.exe client localhost 9000
+_build/default/bin/main.exe server --server-addr localhost --port 9000
+_build/default/bin/main.exe client --server-addr localhost --port 9000
 ```
 
 Format ocaml: ctrl+shift+p, choose ocamlformat manually
--- bin/main.ml
+++ bin/main.ml
@@ -1,45 +1,23 @@
-(* Function to parse command line arguments *)
-let parse_command_line () =
-  match Array.length Sys.argv with
-  | 2 ->
-      let role = Sys.argv.(1) in
-      if role = "server" then ("server", "localhost", !Server.port)
-      else if role = "client" then (
-        Logs.err (fun m -> m "Error: Server address and port not provided.");
-        Logs.err (fun m ->
-            m "Usage: %s <server|client> <server_address> <port>\n" Sys.argv.(0));
-        exit 1)
-      else (
-        Logs.err (fun m ->
-            m "Error: Invalid role specified. Use 'server' or 'client'.\n");
-        exit 1)
-  | 4 ->
-      let role = Sys.argv.(1) in
-      let server_addr = Sys.argv.(2) in
-      let port_str = Sys.argv.(3) in
-      let port_num = int_of_string port_str in
-      if port_num < 0 || port_num > 65535 then (
-        Logs.err (fun m -> m " Error: Invalid port number: %s" port_str);
-        exit 1)
-      else if server_addr <> "localhost" || port_num <> !Server.port then (
-        Logs.err (fun m ->
-            m "Error: Server address and port do not match the server.");
-        exit 1)
-      else (role, server_addr, port_num)
-  | _ ->
-      Logs.err (fun m -> m "Usage: %s <server|client>\n" Sys.argv.(0));
-      exit 1
+open Cmdliner
 
-let print_help () =
-  Logs.info (fun m ->
-      m "Usage: %s <server|client> [server_address port]\n" Sys.argv.(0);
-      Logs.info (fun m ->
-          m
-            "Example:\n\
-             %s server                 # Start the server\n\
-             %s client localhost 9000  # Connect to server at localhost on \
-             port 9000\n\
-            \    " Sys.argv.(0) Sys.argv.(0)))
+type role = Server | Client
+
+let role =
+  let doc = "Specify the role (server or client)." in
+  Arg.(
+    required
+    & pos 0 (some (enum [ ("server", Server); ("client", Client) ])) None
+    & info [] ~docv:"ROLE" ~doc)
+
+let server_addr =
+  let doc = "Server address." in
+  Arg.(
+    value & opt string "localhost"
+    & info [ "s"; "server-addr" ] ~docv:"ADDR" ~doc)
+
+let port =
+  let doc = "Port number." in
+  Arg.(value & opt int 9000 & info [ "p"; "port" ] ~docs:"PORT" ~doc)
 
 let start_client server_addr port =
   Logs.info (fun m -> m "Starting client ...");
@@ -51,35 +29,46 @@ let start_server () =
   let serve = Server.create_server sock in
   Lwt_main.run @@ serve ()
 
-let main () =
+let parse_command_line role server_addr port =
+  match role with
+  | Server -> ("server", server_addr, port)
+  | Client -> ("client", server_addr, port)
+
+let main role server_addr port =
   let () = Logs.set_reporter (Logs.format_reporter ()) in
   let () = Logs.set_level (Some Logs.Info) in
-  match Array.length Sys.argv with
-  | 1 -> print_help ()
-  | 2 -> (
-      match Sys.argv.(1) with
-      | "server" -> start_server ()
-      | "client" ->
-          Logs.err (fun m -> m "Error: Server address and port not provided.");
-          print_help ();
-          exit 1
-      | _ ->
-          Logs.err (fun m ->
-              m "Error: Invalid role specified. Use 'server' or 'client'.\n");
-          print_help ();
-          exit 1)
-  | 4 -> (
-      let role, server_addr, port = parse_command_line () in
-      match role with
-      | "server" -> start_server ()
-      | "client" -> start_client server_addr port
-      | _ ->
-          Logs.err (fun m ->
-              m "Error: Invalid role specified. Use 'server' or 'client'.\n");
-          exit 1)
+  let role, server_addr, port = parse_command_line role server_addr port in
+  match role with
+  | "server" -> start_server ()
+  | "client" -> start_client server_addr port
   | _ ->
-      Logs.err (fun m -> m "Error: Invalid number of arguments.\n");
-      print_help ();
+      Logs.err (fun m ->
+          m "Error: Invalid role specified. Use 'server' or 'client'.\n");
       exit 1
 
-let () = main ()
+let cmd =
+  let doc = "A simple server-client application." in
+  let exits = Cmd.Exit.defaults in
+  let man =
+    [
+      `S "DESCRIPTION";
+      `P "This program starts either a server or a client.";
+      `S "USAGE";
+      `P "$(tname) $(i, ROLE) [$(i, SERVER_ADDR) $(i, PORT)]";
+      `S "OPTIONS";
+      `P "$(b, ROLE) can be either 'server' or 'client'.";
+      `P
+        "$(b, SERVER_ADDR) specifies the server address. Default is \
+         'localhost'.";
+      `P "$(b, PORT) specifies the port number. Default is 9000.";
+      `S "EXAMPLES";
+      `P "$(tname) server                 # Start the server";
+      `P
+        "$(tname) client localhost 9000  # Connect to server at localhost on \
+         port 9000";
+    ]
+  in
+  let term = Term.(const main $ role $ server_addr $ port) in
+  Cmd.v (Cmd.info "server-client" ~doc ~exits ~man) term
+
+let () = exit (Cmd.eval cmd)
