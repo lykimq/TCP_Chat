87fd0087e5ff0a6ad630768838fdaa3c059d6a2a
lykimq <lykimq@gmail.com>
2025-02-03 18:59:30 +0700

    Remove comments

--- lib/client.mli
+++ lib/client.mli
@@ -7,15 +7,11 @@ type t =
 (** Client connection type containing input/output channels and socket *)
 
 val connect_to_server : string -> int -> t Lwt.t
-(** Connect to chat server *)
 
 val send_message : t -> bytes -> unit Lwt.t
-(** Send message to server *)
 
 val start_client : string -> int -> unit Lwt.t
-(** Start client with connection retry logic *)
 
 val stop_client : t -> unit Lwt.t
-(** Stop client gracefully *)
 
 val cleanup_client : t -> unit Lwt.t
