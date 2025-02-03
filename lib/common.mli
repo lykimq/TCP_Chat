7cd93a683e4a74cc8a6713c9a8ecca32a29f6c89
lykimq <lykimq@gmail.com>
2025-02-03 19:04:44 +0700

    Remove comments

--- lib/common.mli
+++ lib/common.mli
@@ -1,10 +1,8 @@
 (** Common functionality for chat client and server *)
 
 val read_message : Lwt_io.input_channel -> Message.t option Lwt.t
-(** Read a message from the input channel *)
 
 val write_message : Lwt_io.output_channel -> Message.t -> unit Lwt.t
-(** Write a message to the output channel *)
 
 val calculate_rtt : float -> float
 (** Calculate round-trip time (RTT) in milliseconds *)
